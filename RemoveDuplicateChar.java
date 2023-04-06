b1568da6f6070a4a107cab638168fd99375fca47
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-04-06 12:59:56 +0530

    Create RemoveDuplicateChar.java

---
+++ RemoveDuplicateChar.java
@@ -0,0 +1,18 @@
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class RemoveDuplicateChar {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter any String");
+		String S = scn.nextLine();
+		Set<Character> s = new LinkedHashSet<Character>();
+		for(int i=0; i<S.length();i++) {
+			s.add( S.charAt(i));
+		}
+		System.out.println(s);
+	}
+
+}
