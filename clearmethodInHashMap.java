36fb419360b6472af8fb4b316e75bce62a8952ea
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-05-03 19:42:24 +0530

    Create clearmethodInHashMap.java

---
+++ clearmethodInHashMap.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class clearmethodInHashMap {
+
+	public static void main(String[] args) {
+		HashMap<String,String>myDetails = new HashMap<String,String>();
+		myDetails.put("FirstName", "Sommanna");
+		myDetails.put("SurName", "Sommanna");
+		myDetails.put("Phone number", "(9304282873");
+		myDetails.put("Password", "##90$$");
+		myDetails.put("DOB", "20/4/1947");
+		myDetails.put("Gender", "Male");
+		
+		System.out.println(myDetails);
+		
+		myDetails.clear();
+		
+		System.out.println(myDetails);
+
+
+	}
+
+}
