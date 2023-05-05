0b8c08e59a15969f693252b0c00bc95e1e8bee18
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-05-05 15:16:04 +0530

    Create ArraySort.java

---
+++ ArraySort.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class ArraySort {
+
+	public static void main(String[] args) {
+
+		int arr[] = {2,6,9,1,0,4,3,7};
+		
+		Arrays.sort(arr);
+		for(int n:arr) System.out.print(n+" ");
+	}
+
+}
