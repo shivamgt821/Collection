c51644e134f256324a473f7450224b2b93f708a9
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-07-27 20:37:32 +0530

    Create ArrayListTrickyQue2.java

---
+++ ArrayListTrickyQue2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+	public class ArrayListTrickyQue2 {
+	  public static void main(String[] args) {
+	    String[] array = new String[]{"A", "B", "C"};
+	    List<String> list1 = Arrays.asList(array);
+	    List<String> list2 = new ArrayList<>(Arrays.asList(array));
+	    List<String> list3 = new ArrayList<>(Arrays.asList("A", new String("B"), "C"));
+	    System.out.println(list1);
+	    System.out.println(list2);
+	    System.out.println(list3);
+	    System.out.print(list1.equals(list2));
+	    System.out.print(list1.equals(list3));
+	    System.out.print(list2.equals(list3));
+	  }
+	}
+
