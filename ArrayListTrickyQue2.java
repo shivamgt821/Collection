2cf6597dd139893505863c4e209f6f2c48396c7f
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-09-12 23:06:46 +0530

    Update ArrayListTrickyQue2.java

--- ArrayListTrickyQue2.java
+++ ArrayListTrickyQue2.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 	 public class ArrayListTrickyQue2  {
-	  public static void main(String[] args) {
+	   public static void main(String[] args) {
 	    String[] array = new String[]{"A", "B", "C"};
 	    List<String> list1 = Arrays.asList(array);
 	    List<String> list2 = new ArrayList<>(Arrays.asList(array));
